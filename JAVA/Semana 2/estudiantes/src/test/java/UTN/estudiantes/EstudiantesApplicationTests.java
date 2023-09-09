ec94c08e3fe343a163dcb0627d5e41cfdae81d51
GabrielNapolitano <95314959+GabrielNapolitano@users.noreply.github.com>
2023-09-09 01:57:12 -0300

    Asistencia
    Asistencia Terror al Ejecutar

---
+++ JAVA/Semana 2/estudiantes/src/test/java/UTN/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package UTN.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PYTHON/Semana 2/tipo_str.py
@@ -0,0 +1,10 @@
+# Profundizando en el tipo string
+# Concatenación automática en python
+
+variable = 'my'
+mensaje = 'hola ''darkness ' + variable
+mensaje += ' old friend'
+# print(mensaje)
+
+# Usamos la clase help para ayuda o documentación build-in incluido
+help(str)
