84d61ab8b77e29bfe992750c4bdf13c32aed2f97
Gabriel <gabrielnapolitano@outlook.com>
2023-10-25 15:16:09 -0300

    Exponencial negativo y positivo.

---
+++ PYTHON/Semana 2/tipo_float.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+# Notación expónencial (valosres positivos o negativos)
+a = 3e5
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier cálculo que incluye un float, todo cambia a float
+
+a = 4.0 + 5
+print(a)
+print(type(a))
