14c64eb176b6a250db27ab3991b04bdb686c1807
Gabriel <gabrielnapolitano@outlook.com>
2023-10-31 18:46:01 -0300

    Asistencia Java.

--- JAVA/Semana 2/estudiantes/src/main/java/UTN/estudiantes/EstudiantesApplication.java
+++ JAVA/Semana 2/estudiantes/src/main/java/UTN/estudiantes/EstudiantesApplication.java
@@ -13,6 +13,43 @@ public class EstudiantesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception{
-		
+
 	}
+
+	Private boolean ejecutarOpciones(Scanner consola){
+		var opcion = Integer.parseInt(consola.nextLine());
+		var salir = false;
+		switch (opcion){
+			case 1 {}
+			case 2 {}
+			case 3 {}
+			case 4 ->{ //Modificar estudiante
+				logger.info("Modificar estudiante: "+nl);
+				logger.info("Ingresar el id estudiante: ");
+				var idEstudiante = Integer.parseInt(consola.nextLine());
+				//buscamos el estudiante a modificar
+				Estudiantes2022 estudiante =
+						estudianteServicio.buscarEstudiantePorId(idEstudiante);
+				if(estudiante != null){
+					logger.info("Nombre: ");
+					var nombre = consola.nextLine();
+					logger.info("Apellido: ");
+					var apellido = consola.nextLine();
+					logger.info("Telefono: ");
+					var telefono = consola.nextLine();
+					logger.info("Email: ");
+					var email = consola.nextLine();
+					estudiante.setNombre(nombre);
+					estudiante.setApellido(apellido);
+					estudiante.setTelefono(telefono);
+					estudiante.setEmail(email);
+					estudianteServicio.guardarEstudiante(estudiante);
+					logger.info("Estudiante modificado: "+estudiante+nl);
+				}
+				else
+					logger.info("Estudiante NO encontrado con el if: "+idEstudiante+nl);
+			}
+		}//Fin Switch
+	}return salir;
+
 }
