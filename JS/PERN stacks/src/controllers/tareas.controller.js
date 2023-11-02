2d601bb2fd108ee3f7738a96e774e3628886d3b5
Gabriel <gabrielnapolitano@outlook.com>
2023-11-02 10:02:55 -0300

    Asistencia javaScript.

--- JS/PERN stacks/src/controllers/tareas.controller.js
+++ JS/PERN stacks/src/controllers/tareas.controller.js
@@ -1,7 +1,15 @@
 export const listarTareas = (req,res) => res.send('obteniendo tareas');
 
-export const listarTarea = (req,res)=> res.send('obteniendo tarea única');
-
+export const listarTarea = async (req,res)=> {
+    const resultado = await pool.query('SELECT = FROM tareas WHERE id =$1',[req.params.id]);
+    if (resultado.rowCount === 0){
+        return res.status(404).json({
+            message: 'La tarea no existe'
+        });
+    }
+    return res.json(resultado.rows[0]);
+}
+    
 export const crearTarea = (req,res)=>res.send('creando tarea');
 
 export const actualizarTarea = (req,res) => res.send('actualizando tarea única');
